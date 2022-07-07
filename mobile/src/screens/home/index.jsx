d428e46eb40db5a81ba272dc5a27533116fa1fad
Yuri Serka <yserka@hotmail.com>
2022-07-06 21:21:09 -0300

    fix: try to make user grant loc permissions on start
    *not perfectly working by now

--- mobile/src/screens/home/index.jsx
+++ mobile/src/screens/home/index.jsx
@@ -1,16 +1,40 @@
+import { Center } from "native-base";
 import { useEffect } from "react";
+import { ActivityIndicator, Linking } from "react-native";
 import LeafletMap from "../../components/Map";
+import AsyncAlert from "../../components/utils/AsyncAlert";
 import { useUserLocation } from "../../store/location/provider";
 
 export default function HomeScreen() {
   const {
-    state: { error },
+    state: { granted, error, loading },
     actions: { requestUserLocation },
   } = useUserLocation();
 
   useEffect(() => {
-    requestUserLocation();
-  }, []);
+    if (loading) return;
 
-  return error ? <Text>{error.message}</Text> : <LeafletMap />;
+    async function setupPermission() {
+      await AsyncAlert(
+        error?.message ?? "ops",
+        "go to settings and grant permission"
+      );
+      await Linking.openSettings();
+      await requestUserLocation();
+    }
+
+    if (!granted) {
+      setupPermission();
+    }
+  }, [loading, granted]);
+
+  if (loading || !granted) {
+    return (
+      <Center flex={1}>
+        <ActivityIndicator />
+      </Center>
+    );
+  }
+
+  return <LeafletMap />;
 }
