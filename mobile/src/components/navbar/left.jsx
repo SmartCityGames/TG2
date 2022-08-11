8b91955d64416c8de7ec7ef05ca1b34446a10acd
Yuri Serka <yserka@hotmail.com>
2022-08-11 01:09:56 -0300

    add: on Avatar click change UserProfilePic

--- mobile/src/components/navbar/left.jsx
+++ mobile/src/components/navbar/left.jsx
@@ -1,20 +1,38 @@
-import { Avatar, Box } from "native-base";
+import { launchImageLibraryAsync, MediaTypeOptions } from "expo-image-picker";
+import { Avatar, Box, Pressable } from "native-base";
 import { useUserProfile } from "../../store/user-profile/provider";
 
 export default function Left(props) {
   const {
     state: { avatar_url },
+    actions: { updateProfilePicture },
   } = useUserProfile();
 
+  async function pickImage() {
+    const result = await launchImageLibraryAsync({
+      mediaTypes: MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+      allowsMultipleSelection: false,
+    });
+
+    if (!result.cancelled) {
+      updateProfilePicture(result.uri);
+    }
+  }
+
   return (
-    <Box {...props} ml="3" mt="3">
-      <Avatar
-        source={{
-          uri: avatar_url,
-        }}
-      >
-        <Avatar.Badge bg="green.500" />
-      </Avatar>
+    <Box {...props} ml="3" mt="5">
+      <Pressable onPress={() => pickImage()}>
+        <Avatar
+          source={{ uri: avatar_url }}
+          borderColor={"black"}
+          borderWidth={1}
+        >
+          <Avatar.Badge bg="green.500" />
+        </Avatar>
+      </Pressable>
     </Box>
   );
 }
